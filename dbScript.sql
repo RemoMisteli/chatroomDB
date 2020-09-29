41806b36ef0c4c08125b79219847edb73e10b9ca
Misteli Remo <rmisteli@fluance.ch>
2020-09-29 17:05:44 +0200

    update

--- dbScript.sql
+++ dbScript.sql
@@ -1,9 +1,18 @@
 CREATE DATABASE chatroom;
 use chatroom;
-CREATE TABLE `messages` (
+CREATE TABLE `messages`(
   `id` int(11) NOT NULL,
+  `user` varchar(255) NOT NULL,
   `content` varchar(255) NOT NULL,
-  PRIMARY KEY (id)
-)
+PRIMARY KEY (id));
+
+
+ALTER TABLE `messages`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+
+INSERT INTO `messages` (`id`, `user`, `content`) VALUES
+(1, 'remo', 'test'),
+(2, 'Fred', 'hey');
+
   
-ALTER TABLE `messages` CHANGE `id` `id` INT(11) NOT NULL AUTO_INCREMENT;
